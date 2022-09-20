a3654d6ed64b3773f732faea484e394c13f39248
이상철 <zlrz6706@gmail.com>
2022-09-20 21:26:10 +0900

    스켈레톤 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
+import Skeleton from './components/Skeleton';
+
 
 const App = () => {
   return (
-    <div>
-      hello skeleton
-    </div>
+      <Skeleton />
   );
 };
 
---
+++ src/components/Skeleton.jsx
@@ -0,0 +1,74 @@
+import React,{useMemo} from 'react';
+import PropTypes from "prop-types"
+import styled,{css, keyframes} from 'styled-components';
+
+const pulseKeyframes = keyframes`
+  0% {
+    opacity : 1;
+  }
+  50% {
+    opacity: 0.4;
+  }
+  100% {
+    opacity : 1;
+  }
+`;
+
+const pulseAnimation = css`
+  animation: ${pulseKeyframes} 1.5s ease-in-out infinite;
+`;
+
+
+const Base = styled.span`
+  ${(props) =>
+    props &&
+      css`
+        background-color: ${props.color};
+      `}
+    border-radius : ${(props) => props.rounded ? "8px" : (props.circle ? "50%" : null)};
+    display: ${(props) => (props.width || props.height) && "block"};
+    width: ${(props) => props.width && props.unit && `${props.width}${props.unit}`};
+    height: ${(props) => props.height && props.unit && `${props.height}${props.unit}`};
+    animation : ${(props) => props.animation && `${pulseAnimation}`};
+`;
+
+const Content = styled.span`
+  opacity: 0;
+`;
+
+const Skeleton = ({
+        animation = true,
+        children,
+        width,
+        height,
+        circle,
+        rounded,
+        count,
+        unit = "px",
+        color = "$F4F4F4",
+    }) => {
+
+  // 만약 count가 4면 "----"를 리턴한다.
+  const content = useMemo(() => [...Array({length:count})].map(() => "-").join(""),[count])
+  return (
+    <Base color='red' width="200px" height="200px">
+      <Content >
+      </Content>
+    </Base>
+  );
+};
+
+export default Skeleton;
+
+
+Skeleton.propsType = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    circle: PropTypes.bool,
+    rounded: PropTypes.bool,
+    count :PropTypes.number,
+    unit: PropTypes.string,
+    animation : PropTypes.bool,
+    color: PropTypes.string,
+}
+
